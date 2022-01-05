43f2fd5721d9b7050675b60fb7de901c0b637304
Deauthorized <49701242+Deauthorized@users.noreply.github.com>
2022-01-05 13:30:06 -0500

    Add files via upload

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: 'Janus',
+    description: 'I manage suggestions and bug reports.',
+    version: '1.0.0',
+    script: 'index.js',
+  }],
+
+  repository: {
+    type: "git",
+    url: "https://github.com/Deauthorized/Janus"
+  }
+};
