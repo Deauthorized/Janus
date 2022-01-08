36a5c86179c69b706505fa66d3dabea4a18e3bf7
Deauthorized <49701242+Deauthorized@users.noreply.github.com>
2022-01-07 23:15:22 -0500

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -5,7 +5,7 @@ const cfg = require('../config.json');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('remove')
-		.setDescription('Remove a thread and its associated message. Staff only.'),
+		.setDescription('Permanently remove a thread and its associated message. Staff only.'),
 	async execute(interaction, client) {
         if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_THREADS)) {
 			await interaction.reply({ content: cfg.noPermission, ephemeral: true });
@@ -20,7 +20,7 @@ module.exports = {
                     .setLabel('Yes, do as I say!')
                     .setStyle('DANGER'),
             );
-            let m = await interaction.reply({ content: `You're about to permanently remove this thread and its parent message. This action will time-out <t:${Math.round(new Date().getTime() / 1000) + 60}:R>. Continue?`, components: [row], ephemeral: true })
+            let m = await interaction.reply({ content: `You're about to permanently remove this thread and its associated message. This request will time out <t:${Math.round(new Date().getTime() / 1000) + 60}:R>. Continue?`, components: [row], ephemeral: true })
     
             const filter = m => m.customId === `destroy-${interaction.channel.id}` && m.user.id === interaction.user.id;
             const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
@@ -41,9 +41,9 @@ module.exports = {
     
                     let mid = await client.channels.cache.get(interaction.channel.parentId).messages.fetch(interaction.channel.id)
     
+                    console.log(`${interaction.user.tag} removed thread #${interaction.channel.name}.`);
                     await interaction.channel.delete({reason: `Thread removed by ${interaction.member.user.username}`});
                     if (mid) {await mid.delete({reason: `Message removed by ${interaction.member.user.username}`});}
-                    console.log(`${interaction.user.tag} removed thread #${interaction.channel.name}.`);
                     return "OKAY";
                 }
 
@@ -84,7 +84,7 @@ module.exports = {
             
             let row = new MessageActionRow().addComponents(opt);
 
-            let m = await interaction.editReply( { content: 'This tool will allow you to permanently delete multiple threads and their associated messages in one action. Please be careful.\n\n', components: [row] } )
+            let m = await interaction.editReply( { content: 'This tool will allow you to permanently delete multiple threads and their associated messages in one action. Please be careful, as it will run as soon as the drop-down is closed.\n\n', components: [row] } )
 
             const filter = m => m.user.id === interaction.user.id;
             const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
