b23452ebf94e6a367fb8994b76084e83db559ce0
Deauthorized <49701242+Deauthorized@users.noreply.github.com>
2022-01-10 14:43:26 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const fs = require('fs');
 const path = require('path');
 const { Client, Collection, Intents, Permissions } = require('discord.js');
 const cfg = require('./config.json');
+const sqlite3 = require('sqlite3');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const commandFiles = fs.readdirSync(path.resolve(__dirname, 'commands')).filter(file => file.endsWith('.js'));
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
 client.commands = new Collection();
+
+let db = new sqlite3.Database('./db/data.sqlite', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    const init = 'CREATE TABLE IF NOT EXISTS issues (id, type, user_id, thread_id, created_at, upvotes, downvotes, bookmark_id)'
+    db.exec(init);
+});
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -19,7 +29,7 @@ for (const file of eventFiles) {
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
 	} else {
-		client.on(event.name, (...args) => event.execute(...args, client));
+		client.on(event.name, (...args) => event.execute(...args, client, db));
 	}
 }
 
@@ -44,7 +54,7 @@ client.on('interactionCreate', async interaction => {
 
 	try {
         console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction. (${interaction})`);
-		console.log(`Interaction returned: ` + await command.execute(interaction, client));
+		console.log(`Interaction returned: ` + await command.execute(interaction, client, db));
 	} catch (error) {
 		console.error(error);
         await interaction.editReply({ content: `Something has gone wrong. \n \`\`\`${error}\`\`\``, ephemeral: true });
